b1282e68b8f0daa11b72302a983073935c58bfbb
Bittu Kumar <bk901277@gmail.com>
2025-05-24 22:10:52 +0530

    Create Model for Project

--- codelifecode/codelifeApp/models.py
+++ codelifecode/codelifeApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class CodeLife(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=250)
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    
