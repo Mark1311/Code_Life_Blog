7af8fffd03c809c3e9dd1fca7f90b35b5ea182b4
Bittu Kumar <bk901277@gmail.com>
2025-05-24 22:21:36 +0530

    model register in admin.py

--- codelifecode/codelifeApp/admin.py
+++ codelifecode/codelifeApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import CodeLife
 # Register your models here.
+
+admin.site.register(CodeLife)
--- codelifecode/codelifeApp/models.py
+++ codelifecode/codelifeApp/models.py
@@ -8,4 +8,12 @@ class CodeLife(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=250)
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    created_to = models.DateTimeField(auto_created=True)
+    updated_to = models.DateTimeField(auto_created=True)
+
+
+
+    def __str__(self):
+        return f'{self.user.username} - {self.test[:10]}'
+    
     
